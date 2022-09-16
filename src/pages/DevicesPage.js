58d44e006ef6beaa2e05aacbac1f36f0f5b184fb
Aleksandr <panarin0290@gmail.com>
2022-09-16 21:30:31 +0300

    test Table in Deviceces page

--- src/pages/DevicesPage.js
+++ src/pages/DevicesPage.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Table from '../components/Table'
 import BasePage from './BasePage'
 
 export default class DevicesPage extends BasePage {
+    constructor(){
+        super()
+        this.data = {
+            head: [1,2,3,4],
+            body: [[1,2,2,2]]
+        }
+    }
   children() {
     return (
-      <div>DevicesPage</div>
+        <>
+            <Table data={ this.data }/>
+        </>
     )
   }
 }
