5aca3b1b3aab06477982c185b5e50c36c030339b
Aleksandr <panarin0290@gmail.com>
2022-09-16 20:17:20 +0300

    add devices page

---
+++ src/pages/DevicesPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from '../components/Container'
+
+export default function DevicesPage() {
+  return (
+    <Container>
+        <div>DevicesPage</div>
+    </Container>
+    
+  )
+}
