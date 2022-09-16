68928ab48b8a2947b74cd5e034cebc7b35487da3
Aleksandr <panarin0290@gmail.com>
2022-09-16 20:51:22 +0300

    delete container

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Container({ children }) {
-  return (
-    <div className='p-6 bg-white rounded-[20px] h-[100%] w-[100%] text-red-500'>{ children }</div>
-  )
-}
