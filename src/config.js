e50d3a6e304882816c483ef620a33ea0636ff557
Aleksandr <panarin0290@gmail.com>
2022-09-16 20:16:24 +0300

    add elements

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import BackupsPage from "./pages/BackupsPage";
+import DevicesPage from "./pages/DevicesPage";
 import MainPage from "./pages/MainPage";
-
+import ManagmentPage from "./pages/ManagmentPage"
+import OptionsPage from "./pages/OptionsPage";
 
 export const config = {
     style: {
@@ -21,13 +23,18 @@ export const config = {
         },
         Management: {
             path: '/mng', 
-            page: '',
+            page: <ManagmentPage />,
             index: 3
+        },
+        Devices: {
+            path: '/devices', 
+            page: <DevicesPage />,
+            index: 4
         },
         Options: {
             path: '/options', 
-            page: '',
-            index: 4
+            page: <OptionsPage />,
+            index: 5
         }
     }
 }
