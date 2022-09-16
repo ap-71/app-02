cac025b52d2055090bd788db49c4f00551c4d601
Aleksandr <panarin0290@gmail.com>
2022-09-16 18:53:48 +0300

    edit navigation block

--- src/components/nav/NavElement.js
+++ src/components/nav/NavElement.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { RoundedTop, RoundedBottom} from './Rounded'
+import { config } from '../../config'
 
-export function NavElement({ to, children, isActive }){
+export function NavElement({ to, children, isActive, elementSize="40px"}){
     return (
-        <div className={`flex w-[150px] rounded-l-full flex-col ${isActive && 'bg-[#FFFFFF] text-[#5459EA]   transition duration-300 delay-300 ease-out'}`}>
-            <Link to={to} className={` h-[40px] px-5 py-2 '}`}>{children}</Link>
-        </div>
-        
+        <>
+            <div className={`relative my-5 flex w-[200px] rounded-l-full flex-col ${isActive ?'bg-[#FFFFFF] text-[#5459EA] transition duration-300 delay-300 ease-out': ''} font-[${config.style.font.family}]`}>
+                <RoundedTop isActive={isActive}></RoundedTop>
+                <RoundedBottom isActive={isActive}></RoundedBottom>
+                <div className={`h-[${elementSize}] flex`}>
+                    <Link to={to} className={`px-7 py-[4px] self-center`}>{children}</Link>
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { NavElement } from './NavElement'
-
+import { config } from '../../config'
 
 export function Navigation(){
   const location = useLocation()
-
-  const menu = {
-    Main: '/',
-    Backups: '/backups',
-    Management: '/mng',
-    Options: '/options',
-  }
+  const paths =  Object.keys(config.menu).filter(key => config.menu[key].page !== '' && config.menu[key].page !== undefined)
+  
   return (<>
-    <nav className='mt-6'>
-      <span>
-        {Object.keys(menu).map((key, index) =>  <NavElement to={menu[key]} key={index} isActive={location.pathname === menu[key]} >{key}</NavElement> )}
-      </span>
+    <nav className='my-10'>
+      <span className={`pl-7 text-white opacity-60 font-[${config.style.font.family}]`}>MAIN MENU</span>
+      <div>
+        { paths.map(key =>  <NavElement to={config.menu[key].path} key={config.menu[key].index} isActive={location.pathname === config.menu[key].path} >{key}</NavElement> ) } 
+      </div>
     </nav>
     </>
   )
